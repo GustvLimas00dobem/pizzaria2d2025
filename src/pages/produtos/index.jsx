ef8f2b9622f51372369d73689298c2d97a64f7af
GustvLimas00dobem <rm95125@estudante.fieb.edu.br>
2025-09-19 09:58:56 -0300

    listagem dinâmica

--- src/pages/produtos/index.jsx
+++ src/pages/produtos/index.jsx
@@ -1,14 +1,23 @@
+import axios from "axios"
+import { useState, useEffect } from "react"
 const Produtos = () => {
+    const [pizzas, setPizzas] = useState([])
+    // Objeto com a lista de pizzas 
+    useEffect(()=>{
+    axios.get("https://viacep.com.br/ws/01010000/json")
+    .then(Response=>{
+        setPizzas(Response.data.data)
+    })
+}, [])
 
+    // Interação da lista de pizzas 
+    const listaPizzas = pizzas.map(pizza=><li>{pizza}</li>)
     return (
         <div>
             <ul>
-                <li>Pizza de Muçarela</li>
-                <li>Pizza de Calabresa</li>
-                <li>Pizza Portuguesa</li>
+                {listaPizzas}
             </ul>
         </div>
     )
 }
-
 export default Produtos
