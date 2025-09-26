af89f30ef5a48a48e31d0e36f506da80241e897f
GustvLimas00dobem <rm95125@estudante.fieb.edu.br>
2025-09-26 10:19:31 -0300

    tabela de cadastro

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,12 @@
 import Produtos from "./pages/produtos"
+import CadastroProduto from "./pages/produtos/cadastro"
 const App = () => {
 
 
   return (
     <>
       <h1>Projeto Pizzaria 2D</h1>
+      <CadastroProduto />
       <Produtos />
       
     </>
